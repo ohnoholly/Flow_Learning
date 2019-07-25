2ca3f48fac56953b88d64484448ab72ab30df84e
Holly Wang <hollywang0728@hotmail.com>
2019-07-25 10:01:27 +0200

    Add the note to the .py file

--- Benchmark/KDD_RF.py
+++ Benchmark/KDD_RF.py
@@ -8,15 +8,19 @@ from sklearn import preprocessing
 
 start_time = time.time()
 
+#Load the data from the CSV file
 data_path = "../../../Dataset/kddcup99.csv"
 dataset = pd.read_csv(data_path, sep=',', usecols=range(0, 42))
 print("Dataset Shape:", dataset.shape)
 
+#Seprerate the dataset for the features and the labels
 input_x = dataset.iloc[:, 0:41]
 input_y = dataset.iloc[:, 41]
 
+#Split the dataset into training and testing set
 train_x, test_x, train_y, test_y = train_test_split(input_x, input_y, test_size=0.20)
 
+# Re-assign the label to only two classes
 new_class = {'back':'abnormal', 'buffer_overflow':'abnormal', 'ftp_write':'abnormal', 'guess_passwd':'abnormal', 'imap':'abnormal',
             'ipsweep':'abnormal', 'land':'abnormal', 'loadmodule':'abnormal', 'multihop':'abnormal', 'neptune':'abnormal', 'nmap':'abnormal',
             'perl':'abnormal', 'phf':'abnormal', 'pod':'abnormal', 'portsweep':'abnormal', 'rootkit':'abnormal', 'satan':'abnormal',
@@ -24,6 +28,7 @@ new_class = {'back':'abnormal', 'buffer_overflow':'abnormal', 'ftp_write':'abnor
 train_y = train_y.replace(new_class)
 test_y = test_y.replace(new_class)
 
+#Encode the label and features to the representative number
 le_y = preprocessing.LabelEncoder()
 le_y.fit(train_y)
 train_y = le_y.transform(train_y)
@@ -41,12 +46,15 @@ for col in test_x.columns:
         le_x.fit(test_x[col])
         test_x[col] = le_x.transform(test_x[col])
 
+#Start training process
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(train_x, train_y)
 
+# Start predicting process
 prid = clf.predict(test_x)
 print(clf.feature_importances_)
 
+#Evaluate the accuracy and the confussion matrix
 print("Accuracy:", clf.score(test_x, test_y))
 
 from sklearn.metrics import confusion_matrix
