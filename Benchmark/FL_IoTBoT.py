cf8f5252157743f7d3f18aee8ee92ae8bafbfff6
Holly Wang <hollywang0728@hotmail.com>
2020-01-13 23:40:35 +0100

    Add the python file

--- Benchmark/FL_IoTBoT.py
+++ Benchmark/FL_IoTBoT.py
@@ -38,20 +38,30 @@ def label_encoder(df):
 BM_DATA_PATH = "../../../Dataset/Botnet_Detection/Philips_B120N10_Baby_Monitor"
 DB_DATA_PATH = "../../../Dataset/Botnet_Detection/Danmini_Doorbell"
 ET_DATA_PATH = "../../../Dataset/Botnet_Detection/Ecobee_Thermostat"
+PT_DATA_PATH = "../../../Dataset/Botnet_Detection/PT_838_Security Camera"
+XC_DATA_PATH = "../../../Dataset/Botnet_Detection/XCS7_1002_WHT_Security_Camera"
 df_bm_b = pd.read_csv(BM_DATA_PATH+"/benign_traffic.csv")
 df_bm_m = pd.read_csv(BM_DATA_PATH+"/Mirai/udp.csv")
 df_db_b = pd.read_csv(DB_DATA_PATH+"/benign_traffic.csv")
 df_db_m = pd.read_csv(DB_DATA_PATH+"/Mirai/udp.csv")
 df_et_b = pd.read_csv(ET_DATA_PATH+"/benign_traffic.csv")
 df_et_m = pd.read_csv(ET_DATA_PATH+"/Mirai/udp.csv")
+df_pt_b = pd.read_csv(ET_DATA_PATH+"/benign_traffic.csv")
+df_pt_m = pd.read_csv(ET_DATA_PATH+"/Mirai/udp.csv")
+df_xc_b = pd.read_csv(XC_DATA_PATH+"/benign_traffic.csv")
+df_xc_m = pd.read_csv(XC_DATA_PATH+"/Mirai/udp.csv")
 
 #Assign the label to each dataframe
 df_bm_b = df_bm_b.assign(label = 'b')
 df_db_b = df_db_b.assign(label = 'b')
 df_et_b = df_et_b.assign(label = 'b')
+df_pt_b = df_pt_b.assign(label = 'b')
+df_xc_b = df_xc_b.assign(label = 'b')
 df_bm_m = df_bm_m.assign(label = 'm')
 df_db_m = df_db_m.assign(label = 'm')
 df_et_m = df_et_m.assign(label = 'm')
+df_pt_m = df_pt_m.assign(label = 'm')
+df_xc_m = df_xc_m.assign(label = 'm')
 
 #Combine the benign traffic and malicious traffic
 df_bm = df_bm_b
@@ -60,6 +70,10 @@ df_db = df_db_b
 df_db = df_db.append(df_db_m, ignore_index = True)
 df_et = df_et_b
 df_et = df_et.append(df_et_m, ignore_index = True)
+df_pt = df_pt_b
+df_pt = df_pt.append(df_pt_m, ignore_index = True)
+df_xc = df_xc_b
+df_xc = df_xc.append(df_xc_m, ignore_index = True)
 
 def shuffler(df):
   return df.reindex(np.random.permutation(df.index))
@@ -68,12 +82,14 @@ def shuffler(df):
 df_bm = shuffler(df_bm)
 df_db = shuffler(df_db)
 df_et = shuffler(df_et)
+df_pt = shuffler(df_pt)
+df_xc = shuffler(df_xc)
 
-# Create a dataset on server for initial model
+# Create a dataset on server for initial model (second version)
 df_server = pd.DataFrame()
-df_server = df_server.append(df_bm.sample(frac =.25), ignore_index=True)
-df_server = df_server.append(df_db.sample(frac =.25), ignore_index=True)
 df_server = df_server.append(df_et.sample(frac =.25), ignore_index=True)
+df_server = df_server.append(df_pt.sample(frac =.25), ignore_index=True)
+df_server = df_server.append(df_xc.sample(frac =.25), ignore_index=True)
 
 #Divide dataframe into x and y
 df_s_x = pd.DataFrame(df_server.iloc[:, 0:115])
@@ -82,35 +98,73 @@ df_bm_x = pd.DataFrame(df_bm.iloc[:, 0:115])
 df_bm_y = pd.DataFrame(df_bm.iloc[:, 115])
 df_db_x = pd.DataFrame(df_db.iloc[:, 0:115])
 df_db_y = pd.DataFrame(df_db.iloc[:, 115])
-df_et_x = pd.DataFrame(df_et.iloc[:, 0:115])
-df_et_y = pd.DataFrame(df_et.iloc[:, 115])
 
 #Normalize the x dataframe
 df_s_x = normalize(df_s_x)
 df_bm_x = normalize(df_bm_x)
 df_db_x = normalize(df_db_x)
-df_et_x = normalize(df_et_x)
 
 #One-Hot encoding labels and transform into array
 s_y = label_encoder(df_s_y)
 bm_y = label_encoder(df_bm_y)
 db_y = label_encoder(df_db_y)
-et_y = label_encoder(df_et_y)
 
+#The different models ready to use:
 class LogisticRegression(torch.nn.Module):
     def __init__(self, input_dim, output_dim):
         super(LogisticRegression, self).__init__()
         self.linear = torch.nn.Linear(input_dim, output_dim)
+        self.bn1 = nn.BatchNorm1d(output_dim)
 
     def forward(self, x):
         outputs = self.linear(x)
         return outputs
 
+class Net(torch.nn.Module):
+    def __init__(self, in_dim, h_dim, out_dim):
+        super(Net, self).__init__()
+        self.linear1 = torch.nn.Linear(in_dim, h_dim)
+        self.bn1 = nn.BatchNorm1d(h_dim)
+        self.linear2 = torch.nn.Linear(h_dim, out_dim)
+
+    def forward(self, x):
+        h_relu = self.linear1(x).clamp(min=0)
+        y_pred = self.linear2(h_relu)
+        return y_pred
+
+
+def logistic_training(epochs, model, data, labels):
+    for epochs in range(int(epochs)):
+        optimizer.zero_grad() ## Zero out the gradient
+        outputs = model(data) ## Call forward
+
+        loss = ((outputs - labels)**2).sum() ## softmax
+        if epochs % 100 == 99:
+            print(loss)
+        loss.backward() ## Accumulated gradient updates into x
+        optimizer.step()
+
+def net_training(epochs, model, data, labels):
+    for e in range(int(epochs)):
+        y_pred = model(data)
+
+        # Compute and print loss
+        loss = criterion(y_pred, labels)
+        if e % 100 == 99:
+            print(e, loss.item())
+
+        # Zero gradients, perform a backward pass, and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+# Create the virtual workers and hook them together
 hook = sy.TorchHook(torch)
 
 BM = sy.VirtualWorker(hook, id='BM')
 DB = sy.VirtualWorker(hook, id='DB')
 
+#Split the dataset itto training and testing
 from sklearn.model_selection import train_test_split
 b_train_x, b_test_x, b_train_y, b_test_y = train_test_split(df_bm_x, bm_y, test_size=0.20)
 d_train_x, d_test_x, d_train_y, d_test_y = train_test_split(df_db_x, db_y, test_size=0.20)
@@ -126,6 +180,9 @@ t_d_test_x = torch.tensor(d_test_x.values.astype(np.float32))
 t_d_train_y = torch.tensor(d_train_y.astype(np.float32))
 t_d_test_y = torch.tensor(d_test_y.astype(np.float32))
 
+n_bm, y_bm = t_b_test_y.shape
+n_db, y_db = t_d_test_y.shape
+
 b_x_train_ptr = t_b_train_x.send(BM)
 b_x_test_ptr = t_b_test_x.send(BM)
 b_y_train_ptr = t_b_train_y.send(BM)
@@ -135,72 +192,81 @@ d_x_test_ptr = t_d_test_x.send(DB)
 d_y_train_ptr = t_d_train_y.send(DB)
 d_y_test_ptr = t_d_test_y.send(DB)
 
-epochs = 3
+#Set up parameters
+epochs = 600
 input_dim = 115
 output_dim = 2 #Number of clasees
-lr_rate = 0.001
+h_dim = 100
+lr_rate = 1e-6
 
-model = LogisticRegression(input_dim, output_dim)
-optimizer = torch.optim.SGD(model.parameters(), lr=lr_rate)
 
-def training(epochs, model, data, labels):
-    print(epochs)
-    for epochs in range(int(epochs)):
-        print("In the loop")
-        optimizer.zero_grad() ## Zero out the gradient
-        outputs = model(data) ## Call forward
-        print(outputs)
-        print(labels)
-        loss = ((outputs - labels)**2).sum() ## softmax
-        print(loss)
-        loss.backward() ## Accumulated gradient updates into x
-        optimizer.step()
+model = torch.nn.Module()
+#Get the input from the user
+model_type = input("Enter the model to use: ")
 
-tensor_server_y = tensor_server_y.squeeze()
-# Train the initial model on Server
-training(epochs, model, tensor_server_x, tensor_server_y)
 
+if model_type == "0":
+    model = LogisticRegression(input_dim, output_dim)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr_rate)
+    # Train the initial model on Server
+    logistic_training(epochs, model, tensor_server_x, tensor_server_y)
+elif model_type == "1":
+    model = Net(input_dim, h_dim, output_dim)
+    criterion = torch.nn.MSELoss(reduction='sum')
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr_rate)
+    net_training(epochs, model, tensor_server_x, tensor_server_y)
+
+
+#Send the initial model to the workers
 BM_model = model.copy().send(BM)
 DB_model = model.copy().send(DB)
+
+print(BM_model)
 
 BM_opt = torch.optim.SGD(params=BM_model.parameters(),lr=lr_rate)
 DB_opt = torch.optim.SGD(params=DB_model.parameters(),lr=lr_rate)
 
+for e in range(100):
 
-for i in range(2):
-
-    # Train Bob's Model
-    BM_opt.zero_grad()
+    #Baby Monitor
+    #print(b_x_train_ptr.get())
     BM_pred = BM_model(b_x_train_ptr)
-    BM_loss = ((BM_pred - b_y_train_ptr)**2).sum()
+
+    # Compute and print loss
+    BM_loss = criterion(BM_pred, b_y_train_ptr)
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    BM_opt.zero_grad()
     BM_loss.backward()
-
     BM_opt.step()
-    BM_loss = BM_loss.get().data
 
-    # Train Alice's Model
-    DB_opt.zero_grad()
+    #Door Bell
     DB_pred = DB_model(d_x_train_ptr)
-    DB_loss = ((DB_pred - d_y_train_ptr)**2).sum()
+
+    # Compute and print loss
+    DB_loss = criterion(DB_pred, d_y_train_ptr)
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    DB_opt.zero_grad()
     DB_loss.backward()
-
     DB_opt.step()
-    DB_loss = DB_loss.get().data
 
-    total_b = 78455
-    correct = 0
-    outputs_b = BM_model(b_x_test_ptr)
-    _b, pred_b = torch.max(outputs_b.data, 1)
-    vb, labels_b = torch.max(b_y_test_ptr.data, 1)
-    correct+= (pred_b == labels_b).sum()
-    accuracy_b = 100*correct/total_b
-    print("Iteration:", i, "BM Accuracy: ", accuracy_b.get().data)
-
-    total_d = 57443
-    correct = 0
-    outputs_d = DB_model(d_x_test_ptr)
-    _d, pred_d = torch.max(outputs_d.data, 1)
-    vd, labels_d = torch.max(d_y_test_ptr.data, 1)
-    correct+= (pred_d == labels_d).sum()
-    accuracy_d = 100*correct/total_d
-    print("Iteration:", i, "DB Accuracy: ", accuracy_d.get().data)
+    if e % 10 == 0:
+        #print(e, "BM_loss:", BM_loss.item())
+        #print(e, "DB_loss:", DB_loss.item())
+        total_b = n_bm
+        correct = 0
+        outputs_b = BM_model(b_x_test_ptr)
+        _b, pred_b = torch.max(outputs_b.data, 1)
+        vb, labels_b = torch.max(b_y_test_ptr.data, 1)
+        correct+= (pred_b == labels_b).sum()
+        accuracy_b = 100*correct/total_b
+        print("Iteration:", e, "BM Accuracy: ", accuracy_b.get().data)
+        total_d = n_db
+        correct = 0
+        outputs_d = DB_model(d_x_test_ptr)
+        _d, pred_d = torch.max(outputs_d.data, 1)
+        vd, labels_d = torch.max(d_y_test_ptr.data, 1)
+        correct+= (pred_d == labels_d).sum()
+        accuracy_d = 100*correct/total_d
+        print("Iteration:", e, "DB Accuracy: ", accuracy_d.get().data)
